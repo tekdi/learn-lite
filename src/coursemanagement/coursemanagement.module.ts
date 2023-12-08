03ed5a78b5f12835d248cf2b49e2410f8c1593c4
Dhanashree-Patil95 <dhanashree.patil@tekditechnologies.com>
2023-12-08 15:01:56 +0530

    Fix:Entity name change

--- src/coursemanagement/coursemanagement.module.ts
+++ src/coursemanagement/coursemanagement.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { CoursemanagementService } from './coursemanagement.service';
 import { CoursemanagementController } from './coursemanagement.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Coursemanagement } from './entities/coursemanagement.entity';
+import { Batchmanagement } from './entities/coursemanagement.entity';
 
 
 @Module({
-  imports : [TypeOrmModule.forFeature([Coursemanagement])],
+  imports : [TypeOrmModule.forFeature([Batchmanagement])],
 
   controllers: [CoursemanagementController],
   providers: [CoursemanagementService],
--- src/coursemanagement/coursemanagement.service.ts
+++ src/coursemanagement/coursemanagement.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBatchemanagementDto } from './dto/create-coursemanagement.dto';
-import { Coursemanagement } from './entities/coursemanagement.entity';
+import { Batchmanagement } from './entities/coursemanagement.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ResponseUtils } from './response-util';
@@ -9,9 +9,9 @@ import { ResponseUtils } from './response-util';
 export class CoursemanagementService {
 
   
-  constructor(@InjectRepository(Coursemanagement) private readonly courseManagementRepository: Repository<Coursemanagement>) {}
+  constructor(@InjectRepository(Batchmanagement) private readonly batchManagementRepository: Repository<Batchmanagement>) {}
 
-  async createBatch(createCoursemanagementDto: CreateBatchemanagementDto):Promise<Coursemanagement> {
+  async createBatch(createBatchmanagementDto: CreateBatchemanagementDto):Promise<Batchmanagement> {
     const {
       courseId,
       name,
@@ -28,7 +28,7 @@ export class CoursemanagementService {
       mentors,
       status,
       tandc,
-  } = createCoursemanagementDto;
+  } = createBatchmanagementDto;
 
     ResponseUtils.validateField(courseId, 'INVALID_COURSE_ID', 'Course does not exist. Please provide a valid course identifier', 'INVALID_COURSE_ID');
     ResponseUtils.validateField(name, 'INVALID_NAME', 'Please provide valid name', 'INVALID_NAME');
@@ -37,7 +37,7 @@ export class CoursemanagementService {
     ResponseUtils.validateField(startDate, 'INVALID_START_DATE', 'Please provide start date', 'INVALID_START_DATE');
     ResponseUtils.validateField(endDate, 'INVALID_END_DATE', 'Please provide end date', 'INVALID_END_DATE');
 
-    const coursemanagement = this.courseManagementRepository.create({
+    const batch = this.batchManagementRepository.create({
       courseId,
       name,
       description,
@@ -55,7 +55,7 @@ export class CoursemanagementService {
       tandc,
   });
 
-  return   await this.courseManagementRepository.save(coursemanagement);
+  return   await this.batchManagementRepository.save(batch);
    }
 
  
